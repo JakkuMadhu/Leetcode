85a4f37e8cde144dd4cbb7b5ebc4867a56af39d8
Jakku Madhu <mj218110@gmail.com>
2025-06-10 23:23:29 +0530

    Time: 1 ms (96.06%) | Memory: 45.7 MB (8.64%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int len=nums.length;
+        int arr[]=new int[len+len];
+
+      for(int i=0;i<len;i++)  
+      {
+        arr[i]=nums[i];
+        arr[i+len]=nums[i];
+      }
+      return arr;
+    }
+}
